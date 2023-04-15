990f3984d0fcae056216755a2b9a6de15c7ed11a
eijjeka <eijjeka@gmail.com>
2023-04-15 14:10:45 +0300

    change readme

--- src/App/shared/Logo/Logo.jsx
+++ src/App/shared/Logo/Logo.jsx
@@ -2,6 +2,9 @@ import { Link } from "react-router-dom";
 import { ContainerImage } from "../ContainerImage";
 import logo from "App/assets/logo.svg";
 
+//please import logo image and add to attribute path, don't forget enter description of image
+//Link - the logo image wrapper where we go
+
 export const Logo = () => {
   return (
     <>
