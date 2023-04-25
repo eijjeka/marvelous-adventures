9c8e59d442dcfd6f8435e7115ebf2baf30e7cf2e
eijjeka <eijjeka@gmail.com>
2023-04-25 22:53:13 +0300

    basename changed

--- src/App/components/Layout/Layout.jsx
+++ src/App/components/Layout/Layout.jsx
@@ -1,15 +1,12 @@
 import { Header } from "./Header";
-// import { Main } from "./Main";
-import { Outlet } from "react-router-dom";
+import { Main } from "./Main";
 import { Footer } from "./Footer";
 
 export const Layout = ({ children }) => {
   return (
     <>
       <Header />
-      <main>
-        <Outlet />
-      </main>
+      <Main>{children}</Main>
       <Footer />
     </>
   );
