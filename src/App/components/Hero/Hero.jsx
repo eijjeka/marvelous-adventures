9b330a5bfc9b809dba227eacff816251fb31bcfd
Saniokkk <88508533+Saniokkk@users.noreply.github.com>
2023-05-02 23:38:25 +0300

    add hero json

--- src/App/components/Hero/Hero.jsx
+++ src/App/components/Hero/Hero.jsx
@@ -2,17 +2,25 @@ import { Container } from "App/shared/Container/Container";
 import { HeroSection } from "App/components/Hero/Hero.styled";
 import { HeroItem } from "./HeroItem";
 import styled from "styled-components";
+import heros from "App/assets/data/hero.json";
+console.log("heros: ", heros);
 
 const SliderList = styled.ul`
   list-style: none;
 `;
 
 export const Hero = () => {
+  const aria = "true";
+
+  const result = aria === "true";
+  console.log("result: ", result);
+
   return (
     <HeroSection padding={"92px 0 80px"} bgc={"#0C0B0B"}>
       <Container>
         <SliderList>
           <HeroItem panther />
+          {heros && heros.map((hero) => <HeroItem {...hero} />)}
         </SliderList>
       </Container>
     </HeroSection>
--- src/App/services/theme.js
+++ src/App/services/theme.js
@@ -6,28 +6,28 @@ const theme = {
   },
 
   panther: {
+    name: "Black Panther",
     color: "#34387F",
     bgGradient:
       "linear-gradient(180deg, #34387F 36.46%, rgba(52, 56, 127, 0) 100%)",
     buttonGradient:
       "radial-gradient(81.15% 81.15% at 50% 18.85%, #34387F 0%, #171717 100%)",
-    buttonText: "Black Panther",
   },
   spider: {
+    name: "Spider man",
     color: "#600404",
     bgGradient:
       "linear-gradient(180deg, #600404 36.46%, rgba(52, 56, 127, 0) 100%)",
     buttonGradient:
       "radial-gradient(81.15% 81.15% at 50% 18.85%, #600404 0%, #171717 100%)",
-    buttonText: "Spider man",
   },
   hulk: {
+    name: "Hulk",
     color: "#5B7F3C",
     bgGradient:
       "linear-gradient(180deg, #5B7F3C 36.46%, rgba(52, 56, 127, 0) 100%)",
     buttonGradient:
       "radial-gradient(81.15% 81.15% at 50% 18.85%, #5B7F3C 0%, #171717 100%)",
-    buttonText: "Hulk",
   },
 };
 
