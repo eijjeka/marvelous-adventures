48a1f20fc7120907f8205bdc95e4d18aabf4b02f
eijjeka <eijjeka@gmail.com>
2023-05-23 01:27:38 +0300

    debounce drop menu done and scroll to section search characters

--- src/App/components/InputSearch/InputSearch.jsx
+++ src/App/components/InputSearch/InputSearch.jsx
@@ -1,12 +1,31 @@
-import { useState, useContext } from "react";
+import { useState, useEffect, useContext } from "react";
 import { SearchContext } from "../Layout/Layout";
+import * as marvelAPI from "App/services/services.js";
 
-import { Container, Input, Button, Svg } from "./InputSearch.styled";
+import {
+  Form,
+  Container,
+  Input,
+  Button,
+  Svg,
+  SearchListContanier,
+  SearchList,
+  SearchListItem,
+} from "./InputSearch.styled";
 import icon from "App/assets/images/sprite.svg";
 
 export const InputSearch = ({ placeholder }) => {
   const { updateQuery } = useContext(SearchContext);
+  const [searchData, setSearchData] = useState([]);
   const [inputValue, setInputValue] = useState("");
+
+  useEffect(() => {
+    const getData = setTimeout(() => {
+      marvelAPI.getCharacterByName(inputValue).then(setSearchData);
+    }, 300);
+
+    return () => clearTimeout(getData);
+  }, [inputValue]);
 
   const handleChange = (event) => {
     setInputValue(event.target.value);
@@ -19,8 +38,14 @@ export const InputSearch = ({ placeholder }) => {
     setInputValue("");
   };
 
+  const handleClickItem = (name) => {
+    updateQuery(name);
+    setInputValue(name);
+    setInputValue("");
+  };
+
   return (
-    <form onSubmit={handleSubmit}>
+    <Form onSubmit={handleSubmit}>
       <Container>
         <Button type="submit" onSubmit={handleSubmit}>
           <Svg>
@@ -33,6 +58,20 @@ export const InputSearch = ({ placeholder }) => {
           value={inputValue}
         />
       </Container>
-    </form>
+      {searchData && searchData.length > 0 && (
+        <SearchListContanier count={searchData.length}>
+          <SearchList count={searchData.length}>
+            {searchData.map((el) => (
+              <SearchListItem
+                key={el.id}
+                onClick={() => handleClickItem(el.name)}
+              >
+                {el.name}
+              </SearchListItem>
+            ))}
+          </SearchList>
+        </SearchListContanier>
+      )}
+    </Form>
   );
 };
